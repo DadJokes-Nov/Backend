e7e7a6ae51f5187b2ec84a4b68993d795e1b2384
Ayomide Jones <ayormeday@gmail.com>
2019-11-18 11:53:08 +0100

    server running

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// const authenticate = require('../auth/authenticate-middleware.js');
+// const authRouter = require('../auth/auth-router.js');
+// const jokesRouter = require('../jokes/jokes-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(logger);
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api/auth', authRouter);
+// server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get("/", (req, res) => {
+    res.send("Server sanity check");
+  });
+  
+  function logger(req, res, next) {
+    console.log(req.method, req.url, Date.now());
+    next();
+  }
+  
+
+module.exports = server;
