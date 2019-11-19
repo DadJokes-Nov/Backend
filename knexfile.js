ca17a1f9abfad994b07b5bc7036c4f2b71701bcb
Ayomide Jones <ayormeday@gmail.com>
2019-11-19 13:13:05 +0100

    edit dbconfig files environment variable to enable heroku access

--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,41 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/jokes.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/jokes.db3" },
     useNullAsDefault: true,
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     },
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" }
   },
 
   testing: {
-    client: 'sqlite3',
-    connection: { filename: './database/jokestest.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/jokestest.db3" },
     useNullAsDefault: true,
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     },
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" }
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations"
     },
-    seeds: { directory: './database/seeds' },
-  },
+    seeds: { directory: "./database/seeds" }
+  }
 };
